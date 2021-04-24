3955016bb3afc2709c2c21351d8b2973a205b5d8
ldy <1185946887@qq.com>
2021-04-24 22:49:54 +0800

    任务处理

---
+++ src/task.c
@@ -0,0 +1,26 @@
+
+
+// 返回当前主机的进程数
+int GetProcessNumber()
+{
+	return 0;
+}
+
+// 返回当前主机的时间
+char *GetHostTime()
+{
+	char  cNowTime[20];
+	
+	return cNowTime;
+}
+
+/*生成一个http响应报文*/
+void GetHttpMessage(char *data, int nbytes)
+{
+
+
+
+	return;
+}
+
+
